8f3c1d6cacbc94dd169d03d9cc36e39695486499
shoomon <kate010117@naver.com>
2024-12-15 03:24:38 +0900

    feature: jwt, spring security 24.12.15

---
+++ src/main/java/com/backend_potato/edubox_team2/global/jwt/JwtFilter.java
@@ -0,0 +1,62 @@
+package com.backend_potato.edubox_team2.global.jwt;
+
+import com.backend_potato.edubox_team2.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private JwtTokenUtil jwtTokenUtil;
+    private final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+
+    public JwtFilter(JwtTokenUtil jwtTokenUtil) {
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String accessToken = resolveToken(request);
+
+        if(accessToken != null && jwtTokenUtil.isValidToken(accessToken)){
+            String userEmail = jwtTokenUtil.getUserEmailFromToken(accessToken);
+            String nickname = jwtTokenUtil.getUserNicknameFromToken(accessToken);
+            String role = jwtTokenUtil.getUserRoleFromToken(accessToken);
+
+            User user = new User();
+            user.setEmail(userEmail);
+            user.setNickname(nickname);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    user, null, AuthorityUtils.createAuthorityList(role)
+            );
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }else{
+            logger.info("유효하지 않은 access 토큰입니다.");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("access-token");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/backend_potato/edubox_team2/global/jwt/JwtTokenUtil.java
@@ -0,0 +1,86 @@
+package com.backend_potato.edubox_team2.global.jwt;
+import com.backend_potato.edubox_team2.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtTokenUtil {
+
+    private final SecretKey SECRET_KEY;
+    private final Logger logger = LoggerFactory.getLogger(JwtTokenUtil.class);
+
+    public JwtTokenUtil(@Value("${spring.auth.jwt.secretKey}") String secretKey) {
+        this.SECRET_KEY = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(User user, String type, Long expireTime) {
+        return Jwts.builder()
+                .claim("type", type)
+                .claim("email",user.getEmail())
+                .claim("nickname", user.getNickname())
+                .claim("role",user.getRole())
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expireTime))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public String generateAccessToken(User user, @Value("${spring.auth.jwt.accessExpireMs}") Long expireTime) {
+        return generateToken(user, "access", expireTime);
+    }
+
+    public String generateRefreshToken(User user, @Value("${spring.auth.jwt.refreshExpireMs}") Long expireTime) {
+        return generateToken(user, "refresh", expireTime);
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUserEmailFromToken(String token) {
+        Claims claims = parseClaims(token);
+        return claims.get("email", String.class);
+    }
+
+    public String getUserNicknameFromToken(String token) {
+        Claims claims = parseClaims(token);
+        return claims.get("nickname", String.class);
+    }
+
+    public String getUserRoleFromToken(String token) {
+        Claims claims = parseClaims(token);
+        return claims.get("role", String.class);
+    }
+
+    public boolean isValidToken(String token){
+        Claims claims;
+        try{
+            claims = parseClaims(token);
+            //토큰 만료가 현재 시간 이전이라면 유효한 토큰
+            return !claims.getExpiration().before(new Date());
+        }catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            logger.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.info("만료된 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+}
