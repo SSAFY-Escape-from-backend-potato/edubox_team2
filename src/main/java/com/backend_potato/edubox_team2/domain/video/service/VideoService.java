dce6c5c84abd0726dcc9c35ba3f1e088e421838d
shoomon <kate010117@naver.com>
2024-12-19 17:27:41 +0900

    feat: 영상 업로드 기본 코드 작성 (todo:video controller 작성, 기타 서비스 로직 추가

--- src/main/java/com/backend_potato/edubox_team2/domain/video/service/VideoService.java
+++ src/main/java/com/backend_potato/edubox_team2/domain/video/service/VideoService.java
@@ -3,9 +3,12 @@ package com.backend_potato.edubox_team2.domain.video.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.S3Client;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
@@ -15,7 +18,17 @@ public class VideoService {
     private final String bucketName = "";
 
     public String uploadVideo(MultipartFile video) throws IOException {
-        String fileName = UUID.randomUUID().toString()+"_"+video.getOriginalFilename();
+        String fileName = UUID.randomUUID().toString() + "_" + video.getOriginalFilename();
 
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+        try (InputStream inputStream = video.getInputStream()) {
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, video.getSize()));
+        }
+
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
     }
 }
